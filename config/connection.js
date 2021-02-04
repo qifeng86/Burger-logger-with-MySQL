8dca20d8f156205f3f64a8b07b514d3cbe629311
Qi Feng <qi.feng86@gmail.com>
2021-02-03 22:00:37 -0500

    created db connection in connections.js

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql")
+
+// create database connection with credentials//
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    questions();
+});
