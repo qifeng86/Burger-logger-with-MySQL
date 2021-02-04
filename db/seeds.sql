96a74249ea05f2c0c43e3ac8b5a63cf6ff06e5b1
Qi Feng <qi.feng86@gmail.com>
2021-02-03 21:52:11 -0500

    created db, table and insert data

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUE ("Cheese Burger", 0);
+INSERT INTO burgers (burger_name, devoured)
+VALUE ("Veggie Burger", 0);
+INSERT INTO burgers (burger_name, devoured)
+VALUE ("Double Patty Burger", 0);
